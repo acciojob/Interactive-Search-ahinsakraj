3ca89af78d23775d55319c134a728f6972c62623
Ahinsak Raj <117904029+ahinsakraj@users.noreply.github.com>
2023-12-23 01:00:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const search=document.querySelector(".search");
+const input=document.querySelector(".input");
+const btn=document.querySelector(".btn");
+
+btn.addEventListener('click',()=>{
+	search.classList.add('active')
+	btn.classList.remove('btn')
+	btn.classList.add('input')
+})
